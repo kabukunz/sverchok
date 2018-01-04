827fd061a4682fe11150e2c0df43c59275fdae4d
zeffii <tetha.z@gmail.com>
2018-01-04 11:12:14 +0100

    add stuff to init

--- core/__init__.py
+++ core/__init__.py
@@ -1,1 +1,24 @@
 
+# this is the list of bl_idname of nodes that use exec or eval 
+
+caution_nodes = {
+    'SvExecNodeMod',
+    'SvSetDataObjectNodeMK2',
+    'SvSetDataObjectNode',
+    'SvBMOpsNodeMK2',
+    'SvBMOpsNode',
+    'EvalKnievalNode',
+    'SvBMVertsNode',
+    'SvScriptNodeLite',
+    'Formula2Node',
+    'SvScriptNode',
+    'SvSetPropNode',
+    'SvGetPropNode',
+    'SvNumpyArrayNode',
+    'SvFormulaShapeNode',
+    'SvSortObjsNode',
+    'FormulaNode',
+    'VoronoiNode',
+    'SvImageComponentsNode',
+    
+}
